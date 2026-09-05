b101fbd0a7fb7d2064903d49f3bb027d3ecd23c1
zila-kh <silasmartboy2015@gmail.com>
2026-09-05 13:27:06 +0700

    feat: expose gallery trash repository

--- src-tauri/src/gallery/mod.rs
+++ src-tauri/src/gallery/mod.rs
@@ -1,6 +1,8 @@
 mod container;
 mod repository;
 mod thumbnail;
+mod trash;
 
 pub use container::ContainerReader;
 pub use repository::{GalleryObject, GalleryPage, GalleryRepository};
+pub use trash::{GalleryTrash, GalleryTrashItem, GalleryTrashPage};
